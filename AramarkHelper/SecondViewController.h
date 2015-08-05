31cd1528a7e1183e9adab2ad705ba83150c37619
Melissa Thai <thaimp@rose-hulman.edu>
2015-08-05 13:37:01 -0700

    Initial commit

---
+++ AramarkHelper/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  AramarkHelper
+//
+//  Created by Melissa Thai on 8/5/15.
+//  Copyright (c) 2015 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ AramarkHelper/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  AramarkHelper
+//
+//  Created by Melissa Thai on 8/5/15.
+//  Copyright (c) 2015 Rose-Hulman. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
